ca2855e345db245a845b613156b12c17cb035ba7
root <root@DESKTOP-87DPNVK.localdomain>
2022-08-20 20:42:20 +0200

    flashing pics

---
+++ src/Card.js
@@ -0,0 +1,14 @@
+import React from 'react';
+const Card = () => {
+	return (
+		<>
+			<div className='first'>
+				<img className='img1' src='./Sandra/Sandy2.png' alt='' />				
+				<img className='img2' src='./Sandra/Sandy6.png' alt='' />				
+				<img className='img3' src='./Sandra/Sandy1.png' alt='' />				
+			</div>
+			<div className="cover"></div>
+		</>
+	);
+}
+export default Card;
