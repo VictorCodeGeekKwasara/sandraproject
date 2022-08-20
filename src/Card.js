fe7423f8fbbb5a3d3b29cb7d515e74e14acbfaee
root <root@DESKTOP-87DPNVK.localdomain>
2022-08-20 21:26:22 +0200

    adding some music

--- src/Card.js
+++ src/Card.js
@@ -3,11 +3,14 @@ const Card = () => {
 	return (
 		<>
 			<div className='first'>
-				<img className='img1' src='./Sandra/Sandy2.png' alt='' />				
-				<img className='img2' src='./Sandra/Sandy6.png' alt='' />				
-				<img className='img3' src='./Sandra/Sandy1.png' alt='' />				
+				<img className='img1' src='./Sandra/Sandy2.png' alt='' />
+				<img className='img2' src='./Sandra/Sandy6.png' alt='' />
+				<img className='img3' src='./Sandra/Sandy1.png' alt='' />
 			</div>
-			<div className="cover"></div>
+			<div className='cover'></div>
+			<audio className="audio" controls autoplay>
+				<source src='./Sandra/Kaskade.mp3' type='audio/mp3'></source>
+			</audio>
 		</>
 	);
 }
